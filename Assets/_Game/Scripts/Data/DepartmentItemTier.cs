a1a2fde1d0be98a99900554d1e30e1fe5513806d
nathan <nathan.h.fisher@gmail.com>
2025-06-05 13:44:50 -0700

    Expand department tiers to Tier1-Tier10

--- Assets/_Game/Scripts/Data/DepartmentItemTier.cs
+++ Assets/_Game/Scripts/Data/DepartmentItemTier.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public enum DepartmentItemTier
 {
-    Basic = 0,
-    Pro = 1,
-    Elite = 2
+    Tier1 = 0,
+    Tier2 = 1,
+    Tier3 = 2,
+    Tier4 = 3,
+    Tier5 = 4,
+    Tier6 = 5,
+    Tier7 = 6,
+    Tier8 = 7,
+    Tier9 = 8,
+    Tier10 = 9
 }
