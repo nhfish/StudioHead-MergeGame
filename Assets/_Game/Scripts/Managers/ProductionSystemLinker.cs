1802819cf3f37d52af381d48ed2d63795768c655
nathan <nathan.h.fisher@gmail.com>
2025-06-10 10:03:30 -0700

    Add DailiesManager for milestone tracking

--- Assets/_Game/Scripts/Managers/ProductionSystemLinker.cs
+++ Assets/_Game/Scripts/Managers/ProductionSystemLinker.cs
@@ -3,17 +3,11 @@ using UnityEngine;
 public class ProductionSystemLinker : MonoBehaviour
 {
     public ProductionManager productionManager;
-    public DistributionQueueManager distributionQueue;
+    public DailiesManager dailiesManager;
 
     void Start()
     {
-        // Register the callback so we know when a movie is done
-        productionManager.OnProductionCompleted.AddListener(HandleProductionComplete);
-    }
-
-    void HandleProductionComplete(MovieRecipe recipe)
-    {
-        Debug.Log(" Production complete! Forwarding movie to distribution queue.");
-        distributionQueue.QueueDistribution(recipe); //  The key line
+        if (dailiesManager != null)
+            dailiesManager.Initialize(productionManager);
     }
 }
